5842beace86222a4c7dcb9ac9b0c05fddd5ec3a2
Aleksey6453 <login6453@gmail.com>
2023-03-20 22:42:02 +0200

    New project about Pizza first_Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import {useState} from 'react'
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-import  MainPage  from './pages/MainPage'
-import AirportDetailsPage from './pages/AirportDetailsPage';
-import AuthPage from './pages/AuthPage';
-import Navigation from './components/Navigation';
+
+
+
 
 
 function App() {
@@ -13,12 +12,7 @@ function App() {
 
   return (
     <div className="main">
-      <Navigation />
-      <Routes>
-        <Route path='/' element= {<MainPage />} />
-        <Route path='/auth' element= {<AuthPage />}/>
-        <Route path='/airport/:id' element= {<AirportDetailsPage />}/>
-      </Routes>
+      <h1>12</h1>
     </div>
   );
 }
